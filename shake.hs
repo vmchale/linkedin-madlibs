6992a37c5622bf518fc78aa82d006b332687ea69
Vanessa McHale <vanessa.mchale@iohk.io>
2018-07-28 22:32:37 -0500

    options

--- shake.hs
+++ shake.hs
@@ -1,5 +1,7 @@
 import           Development.Shake
+import           Development.Shake.Cabal
 import           Development.Shake.ClosureCompiler
+import           Development.Shake.FileDetect
 import           System.Directory
 import qualified System.IO.Strict                  as Strict
 
@@ -11,37 +13,28 @@ main = shakeArgs shakeOptions { shakeFiles = ".shake", shakeLint = Just LintBasi
         cmd ["madlang", "run", "mad-src/linkedin-madlibs.mad", "-r57"]
 
     "clean" ~> do
-        putNormal "cleaning files..."
-        unit $ cmd ["rm", "-rf", "tags"]
+        unit $ cmd ["rm", "-rf", "tags", "mad-src/tags", "build"]
         removeFilesAfter "target" ["//*"]
         removeFilesAfter "dist" ["//*"]
         removeFilesAfter "dist-newstyle" ["//*"]
+        removeFilesAfter ".shake" ["//*"]
 
     "README.md" %> \out -> do
-        hs <- getDirectoryFiles "" ["src//*.hs"]
-        yaml <- getDirectoryFiles "" ["//*.yaml"]
-        cabal <- getDirectoryFiles "" ["//*.cabal"]
-        mad <- getDirectoryFiles "" ["//*.mad"]
+        hs <- getHs ["src"]
+        yaml <- getYml
+        cabal' <- getDirectoryFiles "" ["//*.cabal"]
+        mad <- getMadlang
         html <- getDirectoryFiles "" ["web-src//*.html"]
         css <- getDirectoryFiles "" ["web-src//*.css"]
-        need $ hs <> yaml <> cabal <> mad <> html <> css
+        need $ hs <> yaml <> cabal' <> mad <> html <> css
         (Stdout out') <- cmd ["poly", "-c", ".", "-e", "README.md", "-e", "TODO.md", "-e", "target", "-e", "Justfile", "-e", "CONTRIBUTING.md"]
         file <- liftIO $ Strict.readFile "README.md"
         let header = takeWhile (/= replicate 79 '-') $ lines file
         let new = unlines header ++ out' ++ "```\n"
         liftIO $ writeFile out new
 
-    "purge" ~> do
-        putNormal "purging local files..."
-        unit $ cmd ["rm", "-rf", "tags", "shake", "mad-src/tags"]
-        removeFilesAfter "dist" ["//*"]
-        removeFilesAfter "dist-newstyle" ["//*"]
-        removeFilesAfter ".shake" ["//*"]
-        removeFilesAfter "target" ["//*"]
-
     ["dist-newstyle/build/x86_64-linux/ghcjs-0.2.1.9008011/linkedin-madlibs-0.1.0.0/x/linkedin-madlibs/opt/build/linkedin-madlibs/linkedin-madlibs.jsexe/all.js.externs", "dist-newstyle/build/x86_64-linux/ghcjs-0.2.1.9008011/linkedin-madlibs-0.1.0.0/x/linkedin-madlibs/opt/build/linkedin-madlibs/linkedin-madlibs.jsexe/all.js"] &%> \_ -> do
-        need ["src/Lib.hs","linkedin-madlibs.cabal","cabal.project.local","mad-src/linkedin-madlibs.mad"]
-        unit $ cmd ["bash", "-c", "madlang check mad-src/linkedin-madlibs.mad > /dev/null"]
+        need . snd =<< getCabalDepsA "linkedin-madlibs.cabal"
         cmd ["cabal", "new-build"]
 
     googleClosureCompiler ["dist-newstyle/build/x86_64-linux/ghcjs-0.2.1.9008011/linkedin-madlibs-0.1.0.0/x/linkedin-madlibs/opt/build/linkedin-madlibs/linkedin-madlibs.jsexe/all.js", "dist-newstyle/build/x86_64-linux/ghcjs-0.2.1.9008011/linkedin-madlibs-0.1.0.0/x/linkedin-madlibs/opt/build/linkedin-madlibs/linkedin-madlibs.jsexe/all.js.externs"] "dist-newstyle/build/x86_64-linux/ghcjs-0.2.1.9008011/linkedin-madlibs-0.1.0.0/x/linkedin-madlibs/opt/build/linkedin-madlibs/linkedin-madlibs.jsexe/all.min.js"
@@ -53,8 +46,8 @@ main = shakeArgs shakeOptions { shakeFiles = ".shake", shakeLint = Just LintBasi
     "target/styles.css" %> \out -> do
         liftIO $ createDirectoryIfMissing True "target"
         need ["web-src/styles.css"]
-        cmd ["cp","web-src/styles.css", out]
+        copyFile' "web-src/styles.css" out
 
     "target/index.html" %> \out -> do
         need ["target/all.min.js", "target/styles.css"]
-        cmd ["cp","web-src/index.html", out]
+        copyFile' "web-src/index.html" out
---
+++ src/Main.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+module Main
+    ( main
+    ) where
+
+
+import qualified Data.Map     as M
+import qualified Data.Set     as S
+import           Data.Text    (Text)
+import           Miso
+import           Miso.String
+import           Text.Madlibs (madFile, run)
+
+randomText :: IO Text
+randomText = run $(madFile "mad-src/linkedin-madlibs.mad")
+
+type Model = Text
+
+data Action
+  = Regenerate
+  | Write Text
+  | NoOp
+  deriving (Show, Eq)
+
+main :: IO ()
+main = startApp App {..}
+  where
+    initialAction = NoOp
+    model  = ""
+    update = updateModel
+    view   = viewModel
+    events = defaultEvents
+    subs   = [ keyboardSub keypress ]
+    mountPoint = Nothing
+
+backgroundStyle :: [Attribute action]
+backgroundStyle = [ style_ $ M.fromList [("color", "#4d4d4d"), ("margin-left", "15%"), ("margin-top", "15%") ] ]
+
+largeFont :: [Attribute action]
+largeFont = [ style_ $ M.fromList [("font", "20px \"Comic Sans MS\", Helvetica, sans-serif")] ]
+
+buttonFont :: [Attribute action]
+buttonFont = [ style_ $ M.fromList [("font", "50px \"Comic Sans MS\", Helvetica, sans-serif")] ]
+
+buttonTraits :: [Attribute action]
+buttonTraits = class_ "button" : buttonFont
+
+fontStyles :: [Attribute action]
+fontStyles = [ style_ $ M.fromList [("font", "26px \"Comic Sans MS\", Helvetica, sans-serif")] ]
+
+updateModel :: Action -> Model -> Effect Action Model
+updateModel Regenerate m = m <# fmap Write randomText
+updateModel (Write t) _  = noEff t
+updateModel NoOp m       = noEff m
+
+keypress :: S.Set Int -> Action
+keypress keys = if 83 `elem` S.toList keys then Regenerate else NoOp
+
+viewModel :: Model -> View Action
+viewModel x = div_ backgroundStyle
+    [
+      p_ largeFont [ text "Press 'synergize' or push 's' for another Linkedin article idea" ]
+    , p_ [] [ div_ (onClick Regenerate : buttonTraits) [ text "synergize" ] ]
+    , p_ fontStyles [ text (toMisoString x) ]
+    , p_ [] [ footer ]
+    ]
+
+footerParagraph :: [Attribute action]
+footerParagraph = [ style_ $ M.fromList [("align", "bottom"), ("position", "absolute"), ("bottom", "200px")] ]
+
+footer :: View Action
+footer = footer_ [ class_ "info" ]
+    [ p_ footerParagraph
+        [ a_ [ href_ "https://github.com/vmchale/linkedin-madlibs" ] [ text "source" ] ] ]
